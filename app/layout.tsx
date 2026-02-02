042b02018ff7bda11d19cb0cffc5ee889c7a7754
Kristal_Dev <kristaldev001@gmail.com>
2026-02-02 06:19:17 +0100

    implemented vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto, Lora } from "next/font/google";
 import "./globals.css";
 import Providers from "@/contexts/Providers";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Lakadel",
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} ${playfair.variable} font-sans antialiased`}
       >
+        <Analytics/>
         <Providers>{children}</Providers>
       </body>
     </html>
